96e00b75b2c3830b667f87c1f09dd77a9c81f8bc
msiddiqui <msiddiqui@i2cinc.com>
2023-11-23 00:41:51 +0500

    Bean post construct and pre destroy
    Bean has a complete lifecycle through it goes trough the different phases and generate different callbacks. Among those, the most important ones are post construct and pre-destroy callback
    
    Spring container started -> beans instantiated -> inject dependencies -> internal spring processing -> post construct methods -> "bean ready" ->
    container is shutdown -> pre-destroy callback -> bean destroyed.
    
    Note: When bean scope is set to prototype, then pre-destroy method don't call. neither those beans get destroyed automatically. one must handle in at client side code

--- src/main/java/com/zbrickx/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/zbrickx/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.zbrickx.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -10,8 +12,18 @@ import org.springframework.stereotype.Component;
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
 
+    @PostConstruct
+    public void customStarter(){
+        System.out.println("in post construct" + getClass().getSimpleName());
+    }
+
     CricketCoach(){
         System.out.println("in constructor: "+ getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void customDestroy(){
+        System.out.println("in pre destroy" + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
